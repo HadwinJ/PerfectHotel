3d009eb80ecfa5c7e46d9d7bcbd1b5ae0f72b4a6
hjjiang <hjjiang@beyondh.com>
2018-11-23 16:49:48 +0800

    Add initial website

---
+++ PerfectHotel.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PerfectHotel.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
